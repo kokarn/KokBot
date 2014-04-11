d18ec81e8c743e1298cd0d1154b982964b881f3e
Oskar Risberg <kokarn@gmail.com>
2014-04-11 10:12:01 +0200

    Updated to better follow jslint

--- bot.js
+++ bot.js
@@ -1,56 +1,64 @@
-// Create the configuration
+/*jslint node: true, white: true */
+
 var config = {
-		channels: [ "#kokarn" ],
-		server: "irc.freenode.net",
-		botName: "KokBot"
-	},
-	names = [
-		'kokarn',
-		'gyran'
-	],
-	irc = require( "irc" ),
-	bot = new irc.Client(config.server, config.botName, {
-		channels: config.channels
-	}),
-	mix = "Kokarn: Lägg in dagens mix!";
+        channels: [ "#kokarn" ],
+        server: "irc.freenode.net",
+        botName: "KokBot"
+    },
+    /*
+    names = [
+        'kokarn',
+        'gyran'
+    ],
+    */
+    irc = require( "irc" ),
+    bot = new irc.Client(config.server, config.botName, {
+        channels: config.channels
+    }),
+    mix = "Kokarn: Lägg in dagens mix!";
 
 /*
-// Listen for joins
-bot.addListener("join", function(channel, who) {
-	// Welcome them in!
-	bot.say(channel, who + "...dude...welcome back!");
-});
-*/
+ // Listen for joins
+ bot.addListener("join", function(channel, who) {
+ // Welcome them in!
+ bot.say(channel, who + "...dude...welcome back!");
+ });
+ */
 
 /*
-// Listen for any message, PM said user when he posts
-bot.addListener( "message", function(from, to, text, message) {
-	bot.say(from, "¿Que?");
-});
-*/
-
-// Listen for any message, say to him/her in the room
+ // Listen for any message, PM said user when he posts
+ bot.addListener( "message", function(from, to, text, message) {
+ bot.say(from, "¿Que?");
+ });
+ */
+/*
 bot.addListener( "message", function( from, to, text, message ) {
-	for( var i = 0; i < names.length; i = i + 1 ){
-		if( text.toLowerCase().indexOf( names[ i ] ) !== -1 ){
-			bot.say( config.channels[0], 'Detected highlight "' + names[ i ] + '"' );
-		} else {
-			//console.log( names[ i ] + " in " + text.toLowerCase() + " " + text.toLowerCase().indexOf( names[ i ] ) );
-		}
-	}
+    "use strict";
+    var i;
+    for( i = 0; i < names.length; i = i + 1 ){
+        if( text.toLowerCase().indexOf( names[ i ] ) !== -1 ){
+            //bot.say( config.channels[0], 'Detected highlight "' + names[ i ] + '"' );
+        } else {
+            //console.log( names[ i ] + " in " + text.toLowerCase() + " " + text.toLowerCase().indexOf( names[ i ] ) );
+        }
+    }
 });
+*/
 
 // say todays mix
-bot.addListener("message", function(from, to, text, message) {
-	if (text === "!mix") {
-		bot.say(config.channels[0], mix);
-	}
+bot.addListener( "message", function( from, to, text, message ) {
+    "use strict";
+    if ( text === "!mix" ) {
+        bot.say( config.channels[0], mix );
+    }
 });
 
 // save todays mix
-bot.addListener("message", function(from, to, text, message) {
-	if (text.substring(0, 7) === "!addmix") {
-                mix = text.substring(8);
-	}
+bot.addListener( "message", function( from, to, text, message ) {
+    "use strict";
+    if (text.substring(0, 7) === "!addmix") {
+        mix = text.substring(8);
+        bot.say(config.channels[0], "Tack " + from + " nu blir det dunkadunka!");
+    }
 });
 
