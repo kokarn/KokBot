c9c1de1a8320075bfc8458a58655bfdbdbfdba65
Oskar Risberg <kokarn@gmail.com>
2014-04-14 17:18:07 +0200

    Added pushbullet sending for highlights

--- bot.js
+++ bot.js
@@ -7,7 +7,23 @@ var config = {
     bot = new irc.Client(config.server, config.botName, {
         channels: config.channels
     }),
-    mix = 'Kokarn: Lägg in dagens mix!';
+    mix = 'Kokarn: Lägg in dagens mix!',
+    names = {
+        'kokarn' : 'ujz7gGxaa4adjzWIEVDzOK',
+        'gyran' : 'gustav.ahlberg@gmail.com',
+        'theseal' : 'carlquist@gmail.com'
+    },
+    PushBullet = require( 'pushbullet' ),
+    pusher = new PushBullet( 'v19INs18gL8IWynPqNbmWXns9TErz4k1aBujz7gGxaa4a' );
+
+    PushBullet.prototype.friendNode = function friendNote( email, title, body, callback ) {
+        this.push({
+            email: email,
+            type: 'note',
+            title: title,
+            body: body
+        }, callback);
+    };
 
 /*
  // Listen for joins
@@ -23,19 +39,34 @@ var config = {
  bot.say(from, "¿Que?");
  });
  */
-/*
- bot.addListener( "message", function( from, to, text, message ) {
- "use strict";
- var i;
- for( i = 0; i < names.length; i = i + 1 ){
- if( text.toLowerCase().indexOf( names[ i ] ) !== -1 ){
- //bot.say( config.channels[0], 'Detected highlight "' + names[ i ] + '"' );
- } else {
- //console.log( names[ i ] + " in " + text.toLowerCase() + " " + text.toLowerCase().indexOf( names[ i ] ) );
- }
- }
- });
- */
+
+bot.addListener( 'message#kokarn', function( from, text, message ) {
+    'use strict';
+    var name;
+    for( name in names ){
+        if( names.hasOwnProperty( name ) ){
+            if( text.toLowerCase().indexOf( name ) !== -1 ){
+                bot.say( config.channels[0], 'Detected highlight "' + name + '"' );
+                console.log( 'Trying to push to ' + names[ name ] );
+                if( names[ name ].indexOf( '@' ) !== -1 ){
+                    console.log( 'Notification type: Email' );
+                    pusher.friendNode( names[ name ], 'Message from #kokarn', text, function( error, response ) {
+                        //console.log( 'Error: %j', error );
+                        //console.log( response );
+                        console.log( 'Message sent to ' + names[ name ] );
+                    });
+                } else {
+                    console.log( 'Notification type: Default' );
+                    pusher.note( names[ name ], 'Message from #kokarn', text, function( error, response ){
+                        //console.log( error );
+                        //console.log( response );
+                        console.log( 'Message sent to ' + names[ name ] );
+                   });
+                }
+            }
+        }
+    }
+});
 
 // say todays mix
 bot.addListener( 'message', function( from, to, text, message ) {
