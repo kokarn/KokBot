d101827d1dfb4174bb0df83a0763dab0ed163d5f
Oskar Risberg <kokarn@gmail.com>
2015-05-28 11:06:56 +0200

    Remove extra citations

--- Pushbullet.botplug.js
+++ Pushbullet.botplug.js
@@ -12,11 +12,11 @@ var PushBullet = require( 'pushbullet' ),
     localPushbullet = {
         bot : false,
         nameList : {
-            'kokarn' : 'ujz7gGxaa4adjzWIEVDzOK',
-            'gyran' : 'gustav.ahlberg@gmail.com',
-            'theseal' : 'johancarlquist@gmail.com',
-            'mcgurk' : 'jonathan.wilsson@gmail.com',
-            'gust1n' : 'jocke.gustin@gmail.com'
+            kokarn : 'ujz7gGxaa4adjzWIEVDzOK',
+            gyran : 'gustav.ahlberg@gmail.com',
+            theseal : 'johancarlquist@gmail.com',
+            mcgurk : 'jonathan.wilsson@gmail.com',
+            gust1n : 'jocke.gustin@gmail.com'
         },
         setup : function( bot ){
             var _this = this;
