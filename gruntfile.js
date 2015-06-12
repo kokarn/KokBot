65e0d9df936b163e770f9d9052686defc0345be9
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2015-06-12 13:41:21 +0200

    Check code with jscs

--- gruntfile.js
+++ gruntfile.js
@@ -1,21 +1,27 @@
 module.exports = function( grunt ) {
     'use strict';
 
+    var srcFiles = [
+        'bot.js',
+        '*.botplug.js',
+        'Gruntfile.js'
+    ];
+
     grunt.initConfig({
         pkg: grunt.file.readJSON( 'package.json' ),
         jshint: {
-            all: [
-                'bot.js',
-                '*.botplug.js',
-                'Gruntfile.js'
-            ],
+            all: srcFiles,
             options: {
                 jshintrc: '.jshintrc'
             }
+        },
+        jscs: {
+            src: srcFiles
         }
     });
 
     grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+    grunt.loadNpmTasks( 'grunt-jscs' );
 
     grunt.registerTask( 'test', [ 'jshint' ] );
     grunt.registerTask( 'default', [ 'test' ] );
