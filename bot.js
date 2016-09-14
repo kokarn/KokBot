247769e16f49fa779b83e8fe00cb51d2619a68fc
kokarn <kokarn@gmail.com>
2016-09-14 15:22:40 +0200

    Enable Notifyy McNotifyFace on errors

--- bot.js
+++ bot.js
@@ -1,27 +1,38 @@
 'use strict';
+let Notifyy = require( 'node-notifyy' );
 let irc = require( 'irc' );
 let plugins = require( './plugins' );
 let config = require( './config.js' );
 
-let bot = new irc.Client(
-    config.bot.server,
-    config.bot.botName,
-    {
-        channels: config.bot.channels
-    }
-);
-
-for( let i = 0; i < config.bot.channels.length; i = i + 1 ){
-    console.log( 'Joining', config.bot.channels[ i ], 'as', config.bot.botName );
-}
-
-bot.addListener( 'error', function( message ){
-    console.log( 'error: ', message );
+let notifyy = new Notifyy({
+    users: config.notifyy,
+    title: 'KokBot crashed!'
 });
 
-for( let i = 0; i < config.bot.plugins.length; i = i + 1 ){
-    if( plugins[ config.bot.plugins[ i ] ] ){
-        console.log( 'Starting plugin', config.bot.plugins[ i ] );
-        new plugins[ config.bot.plugins[ i ] ]( bot );
+try {
+    let bot = new irc.Client(
+        config.bot.server,
+        config.bot.botName,
+        {
+            channels: config.bot.channels
+        }
+    );
+
+    for( let i = 0; i < config.bot.channels.length; i = i + 1 ){
+        console.log( 'Joining', config.bot.channels[ i ], 'as', config.bot.botName );
     }
+
+    bot.addListener( 'error', function( message ){
+        console.log( 'error: ', message );
+    });
+
+    for( let i = 0; i < config.bot.plugins.length; i = i + 1 ){
+        if( plugins[ config.bot.plugins[ i ] ] ){
+            console.log( 'Starting plugin', config.bot.plugins[ i ] );
+            new plugins[ config.bot.plugins[ i ] ]( bot );
+        }
+    }
+
+} catch ( error ){
+    notifyy.send( error );
 }
