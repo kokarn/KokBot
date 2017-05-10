7645de616768d37c3589609d347e1566be48634a
kokarn <kokarn@gmail.com>
2017-05-10 09:57:20 +0200

    Disable pushbullet because we don't use it anymore :D

--- plugins/index.js
+++ plugins/index.js
@@ -5,7 +5,7 @@ let urlchecker = require( './urlchecker.js' );
 let github = require( './github.js' );
 let rss = require( './rss.js' );
 let dagensmix = require( './dagensmix.js' );
-let pushbullet = require( './pushbullet.js' );
+// let pushbullet = require( './pushbullet.js' );
 let httpcat = require( './httpcat.js' );
 
 module.exports.telegram = telegram;
@@ -13,5 +13,5 @@ module.exports.urlchecker = urlchecker;
 module.exports.github = github;
 module.exports.rss = rss;
 module.exports.dagensmix = dagensmix;
-module.exports.pushbullet = pushbullet;
+// module.exports.pushbullet = pushbullet;
 module.exports.httpcat = httpcat;
